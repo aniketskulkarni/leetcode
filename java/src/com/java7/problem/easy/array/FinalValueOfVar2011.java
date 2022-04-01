8e6f5d50b5ab19aa95ada8d117c0138aeb5a4c6a
Aniket Kulkarni <aniket16kulkarni@gmail.com>
2022-04-01 10:19:49 +0200

    Final value of Variable operations ++X or --X

---
+++ java/src/com/java7/problem/easy/array/FinalValueOfVar2011.java
@@ -0,0 +1,48 @@
+package com.java7.problem.easy.array;
+
+/**
+ * There is a programming language with only four operations and one variable X:
+ * 
+ * ++X and X++ increments the value of the variable X by 1.
+ * --X and X-- decrements the value of the variable X by 1.
+ * Initially, the value of X is 0.
+ * 
+ * Given an array of strings operations containing a list of operations, return
+ * the final value of X after performing all the operations.
+ *
+ * Example 1:
+ * 
+ * Input: operations = ["--X","X++","X++"]
+ * Output: 1
+ * Explanation: The operations are performed as follows:
+ * Initially, X = 0.
+ * --X: X is decremented by 1, X = 0 - 1 = -1.
+ * X++: X is incremented by 1, X = -1 + 1 = 0.
+ * X++: X is incremented by 1, X = 0 + 1 = 1.
+ */
+
+public class FinalValueOfVar2011 {
+
+    // Time: O(n)| Space: O(1)
+    public int finalValueAfterOperations(String[] operations) {
+        if (operations == null || operations.length == 0) {
+            return -1;
+        }
+
+        int x = 0;
+
+        for (int index = 0; index < operations.length; index++) {
+            char[] charArr = operations[index].toCharArray();
+            for (int chIdx = 0; chIdx < charArr.length; chIdx++) {
+                if (charArr[chIdx] == '+') {
+                    x += 1;
+                    break;
+                } else if (charArr[chIdx] == '-') {
+                    x -= 1;
+                    break;
+                }
+            }
+        }
+        return x;
+    }
+}
